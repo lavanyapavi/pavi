880b5ed7b81e8606495c7c88093a3134b9ee60ea
lavanyapavi <lavanyav2979@gmail.com>
2017-06-29 11:45:48 +0530

    Add files via upload

---
+++ String.java
@@ -0,0 +1,20 @@
+package pavi;
+import java.util.*;
+public class String {
+	public static void main(String[] args) {
+	java.lang.String str="madam";
+	String reverse,len;
+	for(int i=len-1;i>-1;i--)
+	{
+		reverse=reverse+Str.charAt(i);
+	}
+	System.out.println("reverse string is"+reverse);
+}
+
+	/**
+	 * @param args
+	 */
+	
+		// TODO Auto-generated method stub
+
+	}
