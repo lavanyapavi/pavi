900a3e9871be9461e7c1e5248a591365f6eb5681
lavanyapavi <lavanyav2979@gmail.com>
2017-06-29 12:05:20 +0530

    Add files via upload

---
+++ String3.java
@@ -0,0 +1,15 @@
+package Priya;
+import java.util.*;
+public class String3 {
+	public static void main(String[] args){
+		int rev,n;
+		Scanner s=new Scanner(System.in);
+		while(n>0)
+		{
+			rev=n%10;
+			n=n/10;
+		}
+		System.out.println("reverse digit"+rev);
+			}
+	}
+
