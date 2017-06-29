db8db173bb56800c95acb50b0945c014ed1a51fc
lavanyapavi <lavanyav2979@gmail.com>
2017-06-29 11:56:15 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,14 @@
+package Priya;
+import java.util.*;
+public class String2 {
+int n,fact=1;
+Scanner s=new Scanner(System.in);
+{
+n=s.nextInt();
+for(i=1;i<=n;i++)
+	fact=fact*i;
+{
+	System.out.println("factorial"+fact);
+}
+
+
